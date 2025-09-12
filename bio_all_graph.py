c0047ed4eebd70e0d37d0952c1b4fe97aaae2f4c
AchalaC8 <chandruachala9@gmail.com>
2025-09-12 11:41:33 +0530

    analyse all with graph

--- bio_all_graph.py
+++ bio_all_graph.py
@@ -7,19 +7,23 @@ for i in os.listdir():
     if(i.endswith('.csv')):
         print("Day",j,"analysis")
         a=pd.read_csv(i)
-
-        print('Count of Present vs Not Present')
-        print(a['Status'].value_counts())
-
+    
         print('Total students')
         print(len(a))
+        print('Count of Present vs Not Present')
+        print(a['Status'].value_counts())
 
         print('Students per floor')
         print(a['Floor'].value_counts())
 
         print('Students per room')
         print(a['Room No.'].value_counts())
-
         print("\n\n\n")
         j+=1
+        a['Status'].value_counts().plot.pie(autopct='%1.1f%%', startangle=90, colors=['lightgreen', 'lightcoral'])
+        plt.title('Overall Attendance')
+        plt.ylabel('')
+        plt.show()
+
+
 input()
